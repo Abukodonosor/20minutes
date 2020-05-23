6be00461036419db396d77e9684133abd8f2071e
Abukodonosor <cube.uros@gmail.com>
2020-05-23 15:22:37 +0200

    Fix some tests

--- src/server/model/post/test/post.test.js
+++ src/server/model/post/test/post.test.js
@@ -1,6 +1,8 @@
 const Post = require("../Post");
 
 describe("Post model verification", () => {
+  it("Create Post", async () => {});
+
   it("List all posts", async () => {
     const allPosts = Post.getAll();
     expect(allPosts.length).not.toBe(0);
