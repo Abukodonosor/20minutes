7cd0cb0e8efd967742d0e5d54a70acf7b41510ec
Abukodonosor <cube.uros@gmail.com>
2020-05-24 03:09:51 +0200

    Implement blogItemCard component

--- src/client/components/MainPage/index.jsx
+++ src/client/components/MainPage/index.jsx
@@ -1,9 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const MainPage = () => {
+  const blogItemsList = mockBlogItems.map((el, key) => (
+    <BlogItemCard {...el} />
+  ));
+
+  return <div className="main-page">{blogItemsList}</div>;
+};
+
+const BlogItemCard = ({ title, numberOfComments, linkToPost }) => {
   return (
-    <div className="main-page">place where all posts need to be listed</div>
+    <div class="blog-item">
+      <h4>{title}</h4>
+      <span>comments per post:{numberOfComments}</span>
+      <br />
+      <Link to={linkToPost}>{linkToPost}</Link>
+    </div>
   );
 };
 
-const BlogItemCard = () => {};
+const mockBlogItems = [
+  {
+    title: "Some new post",
+    numberOfComments: 32,
+    linkToPost: "https://reacttraining.com/react-router/web/guides/quick-start",
+  },
+  {
+    title: "Some new post",
+    numberOfComments: 32,
+    linkToPost: "https://reacttraining.com/react-router/web/guides/quick-start",
+  },
+  {
+    title: "Some new post",
+    numberOfComments: 32,
+    linkToPost: "https://reacttraining.com/react-router/web/guides/quick-start",
+  },
+  {
+    title: "Some new post",
+    numberOfComments: 32,
+    linkToPost: "https://reacttraining.com/react-router/web/guides/quick-start",
+  },
+  {
+    title: "Some new post",
+    numberOfComments: 32,
+    linkToPost: "https://reacttraining.com/react-router/web/guides/quick-start",
+  },
+  {
+    title: "Some new post",
+    numberOfComments: 32,
+    linkToPost: "https://reacttraining.com/react-router/web/guides/quick-start",
+  },
+];
